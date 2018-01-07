4883ad086bc8018f7b2cc0e9674fe92a8d378717
bksaas <bksaas@coding.net>
2018-01-07 20:54:18 +0800

    feature: add flush readme script

---
+++ flush_readme.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo """# 刷题喽~
+
+  本工程单纯用来维护刷题的代码，自娱自乐~
+""" > ./README.md
+
+for f in *.py;
+do
+     if [[ $f =~ ^[0-9]{1,}.*.py ]]
+     then
+        echo "## $f" >> ./README.md
+     else
+        echo "skipped: $f"
+     fi
+done
